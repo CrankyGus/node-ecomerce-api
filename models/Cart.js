e7deecda9c17af5c3572e8312ff6f550ac151880
CrankyGus <jirateep.loyma02@gmail.com>
2022-12-26 19:50:06 +0700

    add - models && json status

---
+++ models/Cart.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, require: true },
+    products: [
+      {
+        productID: { type: String },
+        quantity: { type: Number, default: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", CartSchema);
