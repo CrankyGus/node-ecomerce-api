ad69cacc7fba9a783ef65e399b9f7f75123255d4
CrankyGus <jirateep.loyma02@gmail.com>
2022-12-29 14:50:10 +0700

    add - routes && enable cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const userRoute = require("../routes/user");
-const authRoute = require('../routes/auth')
+const mongoose = require('mongoose');
+const userRoute = require('../routes/user');
+const authRoute = require('../routes/auth');
+const productRoute = require('../routes/product');
+const cartRoute = require('../routes/cart');
+const orderRoute = require('../routes/order');
+const cors = require('cors')
 
 const port = process.env.PORT;
 
@@ -15,9 +19,13 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
-app.use("/api/users", userRoute);
-app.use('/api/auth', authRoute)
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
 
 app.listen(port, () => {
   console.log(`[INFO]: Server Starting...`);
